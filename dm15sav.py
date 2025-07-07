caaccab3f7a7eba81834b3ef96d5a8a9226c908f
My Name <my-name@chromium.org>
2025-07-07 17:02:29 -0400

    Fixed path typo and tested the script.

--- dm15sav.py
+++ dm15sav.py
@@ -3,9 +3,9 @@ import pandas as pd
 # Define column names based on your dm15.dat file structure
 col_names = ['SN_name', 'SN_dm15', 'SN_dm15source', 'SN_datasource', 'SN_z', ...] 
 
-dm15_df = pd.read_csv(r'C./dm15.dat', 
+dm15_df = pd.read_csv(r'./dm15.dat', 
                       names=col_names, 
                       delimiter=',')
 
 # You can now easily access data, e.g.:
-# print(dm15_df[dm15_df['SN_name'] == '1998aq'])
+# print(dm15_df)
