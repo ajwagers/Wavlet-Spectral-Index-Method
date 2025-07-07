a50f1d9f4cecf88974422995b8313ccf60a951e0
My Name <my-name@chromium.org>
2025-07-07 16:57:43 -0400

    Starting a reimplimentation of the Wavelet Spectral Index Method using Python

---
+++ dm15sav.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Define column names based on your dm15.dat file structure
+col_names = ['SN_name', 'SN_dm15', 'SN_dm15source', 'SN_datasource', 'SN_z', ...] 
+
+dm15_df = pd.read_csv(r'C:\Users\ajwag\Documents\Code\TAMU\TAMU_research_old\IDL_Archive\dm15.dat', 
+                      names=col_names, 
+                      delimiter=',')
+
+# You can now easily access data, e.g.:
+# print(dm15_df[dm15_df['SN_name'] == '1998aq'])
